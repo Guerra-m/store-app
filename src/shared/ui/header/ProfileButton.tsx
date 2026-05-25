06ba04fe50b0de11d12650ca886fd5de9f78e26e
Martin Guerra Local <vernonguerra27@gmail.com>
2026-05-25 16:58:50 -0300

    ADD: modal profile

--- src/shared/ui/header/ProfileButton.tsx
+++ src/shared/ui/header/ProfileButton.tsx
@@ -1,36 +1,25 @@
-export const ProfileButton = () => {
+type Props = {
+  onClick: () => void;
+};
 
+export const ProfileButton = ({ onClick }: Props) => {
   return (
-
     <button
+      onClick={onClick}
       className="
-        flex
-        items-center
-        justify-center
-
-        w-11
-        h-11
-
+        flex items-center justify-center
+        w-11 h-11
         rounded-xl
-
         bg-surface-container-low
-
-        border
-        border-outline-variant
-
+        border border-outline-variant
         text-on-surface
-
         hover:bg-surface-container
-
-        transition-all
-        cursor-pointer
+        transition-all cursor-pointer
       "
     >
-
       <span className="material-symbols-outlined">
         person
       </span>
-
     </button>
   );
 };
